e8b504e4951f74139b39b5e8f2ed575c92d9edb2
Olha Vasyletska <olha.vasyletska@playtech.com>
2016-03-19 14:07:38 +0200

    updating the project

--- project/js/slider/slider.js
+++ project/js/slider/slider.js
@@ -9,14 +9,13 @@ function Slider(sSelector) {
 	s.image = s.find('#img');
 	s.prev = s.find('#left');
 	s.next = s.find('#right');
-	s.bullets = s.find('.sliderBullets>ul>li');
+	s.bullets = s.find('.sliderBullet');
 	
 	
 	s.ticker = null;
 	s.imageCount = 1;
 	s.total = 4;
 	s.seconds = 5000;
-	s.bullet = null;
 
 	
 //-------------------methods---------------------
@@ -32,26 +31,32 @@ function Slider(sSelector) {
 			s.imageCount = s.total;
 		}
 		s.image.attr('src', 'images/slider' + s.imageCount + '.png');
+        
+		
+        s.bullets.removeClass('active');
+        s.bullets.eq(s.imageCount-1).addClass('active');
 
 	}
-	
+    
 
+    
 	s.bulletClick = function(event) {
-		
 		s.stopAutoSlider();
+        
 		s.imageCount = 1;
 		s.imageCount = s.bullets.index(event.target) + 1;
-		var currentBullet = s.bullets[s.imageCount - 1];
-		
+        
 		s.bullets.removeClass('active');
 		$(this).addClass('active');
-		
+        
 		s.image.attr('src', 'images/slider' + s.imageCount + '.png');
+        
 		s.autoSlide();
 	}
 	
 	
 	s.showPrev = function() {
+        
 		s.stopAutoSlider();
 		s.slide(-1);
 		s.autoSlide();
@@ -59,7 +64,8 @@ function Slider(sSelector) {
 	
 	
 	s.showNext = function() {
-		s.stopAutoSlider();
+
+        s.stopAutoSlider();
 		s.slide(1);
 		s.autoSlide();
 	}
