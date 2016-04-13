ffb0200962449761acbbde5145468c616fa77468
OlhaV <olga4irkova@gmail.com>
2016-04-13 16:21:33 +0300

    Added files via upload

--- project/js/calculator/calculator.js
+++ project/js/calculator/calculator.js
@@ -11,14 +11,42 @@ function Calculator(sSelector) {
 	c.equals = c.calculator.find('.equals');
 	c.result = c.calculator.find('#result');
 	c.buttons = c.calculator.find('.button');
+    
+    var calculated = false;
 	
 	
 	/* ----------------- methods ------------------- */	
 	
+    c.display = function(event) {
+        if(event.target.innerHTML != '=') {
+            c.result.append($(this).html());
+        } if (calculated) {
+            c.result.html($(this).html());
+            calculated = false;
+        }
+    
+    }    
+    
+    
+    c.clearResult = function() {
+        c.result.html('');
+    }
+    
+    
+    c.calculate = function(event) {
+        var result = c.result.html();
+        var res = eval(result);
+        c.result.html(res);
+        
+        calculated = true;
+    }
+    
+    
 	c.push = function() {
 		$(this).addClass('clicked');
 	}
 	
+    
 	c.release = function() {
 		$(this).removeClass('clicked');
 	}
@@ -27,10 +55,14 @@ function Calculator(sSelector) {
 	
 	/* ------------------ events ------------------- */	
 	
+    c.buttons.click(c.display);
+    
+    
+	c.equals.click(c.calculate);
+	c.clear.click(c.clearResult);
+	
 	c.buttons.mousedown(c.push);
 	c.buttons.mouseup(c.release);
-	
-	
-	
+    c.buttons.mouseleave(c.release);
 	
 }
