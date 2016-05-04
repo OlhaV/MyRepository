913e138d7e75657e604781f5f195a6e0bde3ba86
OlhaV <olga4irkova@gmail.com>
2016-05-04 12:04:26 +0300

    Added files via upload

--- project/js/calculator/calculator.js
+++ project/js/calculator/calculator.js
@@ -6,39 +6,114 @@ function Calculator(sSelector) {
 	c.calculator = $(sSelector);
 	
 	c.numbers = c.calculator.find('.number');
-	c.operators = c.calculator.find('.operator');
+	c.operators = ['+', '-', '*', '/'];
+    c.operatorButtons = c.calculator.find('.operator');
+    c.dot = c.calculator.find('.dot');
 	c.clear = c.calculator.find('.clear');
 	c.equals = c.calculator.find('.equals');
 	c.result = c.calculator.find('#result');
-	c.buttons = c.calculator.find('.button');
+	c.buttons = c.calculator.find('p');
+    c.keys = c.calculator.find('.key');
     
-    var calculated = false;
-	
-	
+    c.dotAdded = false;
+    c.errorMessage = 'You cannot devide by zero';
+    
 	/* ----------------- methods ------------------- */	
-	
-    c.display = function(event) {
-        if(event.target.innerHTML != '=') {
-            c.result.append($(this).html());
-        } if (calculated) {
-            c.result.html($(this).html());
-            calculated = false;
+    
+    /* when you click on number */ 
+    c.numberClicked = function() {
+        
+        /* if there is a message in the result field, first it is erased */
+        if(c.result.html() == c.errorMessage) {
+            c.clearResult();
         }
+        /* then a number added */
+        c.result.append($(this).html());
+    }
     
-    }    
+    /* when you click on operator */
+    c.operatorClicked = function() {
+        c.dotAdded = false;
+        /* check if the result field is not empty*/
+        if(c.result.html()) {
+        
+            /* check if the last character is an operator or a dot */
+            if(c.operators.indexOf(c.result.text().slice(-1)) > -1 || c.result.html().slice(-1) == '.') {
+//как сократить эту часть? 
+                var result = c.result.html().slice(0, c.result.html().length - 1);
+                c.result.html(result);
+                c.result.append($(this).html());
+
+    //            почему не работает replace?
+    //            c.result.html().replace(c.result.text().slice(-1), $(this).html());
+            }
+            
+            /* if there is a message in the result field, first it is erased */
+            else if(c.result.html() == c.errorMessage) {
+                    c.clearResult();
+                }
+
+            else {
+                c.result.append($(this).html());
+            }
+        }
+        
+        /* check if the result field is empty */
+        else if($(this).html() == '-') {
+            c.result.append($(this).html());
+        }
+    }
     
+    /* when you click on a dot */
+    c.dotClicked = function() {
+        /* check if there is no dot yet in the result field and if the last symbol is not an operator*/
+        if(!c.dotAdded) {
+            /* check if the result field is not empty */
+            if(c.result.html()) {
+                c.result.append('.');
+            } 
+            /* if the result field is empty, append zero+dot */
+            else {
+                c.result.append('0.');
+            }
+            
+            c.dotAdded = true;
+        }
+    }
     
+    /* clear the result field */
     c.clearResult = function() {
+        c.dotAdded = false;
         c.result.html('');
     }
     
-    
-    c.calculate = function(event) {
-        var result = c.result.html();
-        var res = eval(result);
-        c.result.html(res);
+    /* calculate the expression in the result field */
+    c.calculate = function() {
+        /* check if you try to devide by zero */ 
+        if(c.result.html().slice(-2) == '/0') {
+            /* display in the result field the message */
+            c.result.html(c.errorMessage);
+        } 
         
-        calculated = true;
+        /* check if the last symbol is operator or dot */
+        else if(c.operators.indexOf(c.result.html().slice(-1)) > -1 || c.result.html().slice(-1) == '.') {
+            
+        /* remove the last symbol from the result field */     
+            c.result.html(c.result.html().slice(0, c.result.html().length - 1));
+        }
+        
+        /* else - calculate */
+        if(c.result.html() && c.result.html() != c.errorMessage) {
+            c.result.html(eval(c.result.html()));     
+        }
+        
+        /* change the value of c.dotAdded property to "false" if there is no dot in calculated result */
+        if(c.result.html().indexOf('.') > -1) {
+            c.dotAdded = true;
+        }
+        else {
+            c.dotAdded = false;
+        }
     }
     
     
@@ -55,8 +130,9 @@ function Calculator(sSelector) {
 	
 	/* ------------------ events ------------------- */	
 	
-    c.buttons.click(c.display);
-    
+    c.numbers.click(c.numberClicked);
+    c.operatorButtons.click(c.operatorClicked);
+    c.dot.click(c.dotClicked);
     
 	c.equals.click(c.calculate);
 	c.clear.click(c.clearResult);
@@ -66,3 +142,11 @@ function Calculator(sSelector) {
     c.buttons.mouseleave(c.release);
 	
 }
+
+
+
+
+
+
+
+
